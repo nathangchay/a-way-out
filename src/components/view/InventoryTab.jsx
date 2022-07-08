3e6e8601b6ced1562472cf16fcc76b7b1b2cee3e
nate <nathangchay@gmail.com>
2022-07-08 03:15:38 -0600

    Create InventoryTab.jsx

---
+++ src/components/view/InventoryTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Typography } from '@rmwc/typography';
+
+function InventoryTab() {
+  return (
+    <Typography use="body1">This is the inventory tab</Typography>
+  );
+}
+
+export default InventoryTab;
