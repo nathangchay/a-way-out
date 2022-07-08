7511ee758fa4c9a44eb3a0da0a89b0fc6f55a85f
nate <nathangchay@gmail.com>
2022-07-08 13:32:51 -0600

    Add reward props to searchers

--- src/components/view/RoomTab.jsx
+++ src/components/view/RoomTab.jsx
@@ -7,13 +7,13 @@ import Searcher from '../controller/Searcher';
 function RoomTab() {
   return (
     <>
-      <Typography use="body1" className="body1">in this room: </Typography>
-      <Searcher name="dirt pile" duration={1} />
-      <Searcher name="dirt pile" duration={2} />
-      <Searcher name="dirt pile" duration={3} />
-      <Searcher name="dirt pile" duration={4} />
-      <Searcher name="cabinet" duration={5} />
-      <Searcher name="bed" duration={10} />
+      <Typography use="headline6" className="tab-header">in this room: </Typography>
+      <Searcher name="dirt pile" duration={1} reward="key" rewardAmount={1} />
+      <Searcher name="dirt pile" duration={2} reward="bread" rewardAmount={2} />
+      <Searcher name="dirt pile" duration={3} reward="coin" rewardAmount={10} />
+      <Searcher name="dirt pile" duration={4} reward="coin" rewardAmount={10} />
+      <Searcher name="cabinet" duration={5} reward="coin" rewardAmount={10} />
+      <Searcher name="bed" duration={10} reward="coin" rewardAmount={10} />
     </>
   );
 }
