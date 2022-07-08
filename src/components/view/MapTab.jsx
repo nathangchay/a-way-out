5dbab629966e37bb6ea6630729aaadee503a0171
nate <nathangchay@gmail.com>
2022-07-08 03:15:40 -0600

    Create MapTab.jsx

---
+++ src/components/view/MapTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Typography } from '@rmwc/typography';
+
+function MapTab() {
+  return (
+    <Typography use="body1">This is the map tab</Typography>
+  );
+}
+
+export default MapTab;
