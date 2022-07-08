fa8b585a22e681d5f3a6c42c2fa8741b10f9eab8
nate <nathangchay@gmail.com>
2022-07-08 13:32:33 -0600

    Add item to inventory, fix timing

--- src/components/controller/Searcher.jsx
+++ src/components/controller/Searcher.jsx
@@ -4,20 +4,23 @@ import { Typography } from '@rmwc/typography';
 import { Button } from '@rmwc/button';
 import { LinearProgress } from '@rmwc/linear-progress';
 
-function Searcher({ name, duration }) {
+import { addToInventory } from '../model/Inventory';
+
+function Searcher({
+  name, duration, reward, rewardAmount,
+}) {
   let progress = 0;
 
   const [progressState, setProgressState] = React.useState(progress);
   const [disabled, setdisabled] = React.useState(false);
 
   const searchLoop = () => {
-    if (progress >= 1) {
+    if (progress > 1 - (1 / duration)) {
       setdisabled(false);
+
       progress = 0;
       setProgressState(0);
     } else {
-      setdisabled(true);
-
       setTimeout(searchLoop, 1000);
       progress += 1 / duration;
 
@@ -25,10 +28,17 @@ function Searcher({ name, duration }) {
     }
   };
 
+  const onButtonClick = () => {
+    addToInventory(reward, rewardAmount);
+
+    setdisabled(true);
+    searchLoop();
+  };
+
   return (
     <div className="container-searcher">
       <Typography use="body2" style={{ minWidth: '20%' }}>{name}</Typography>
-      <Button raised disabled={disabled} label="search" style={{ margin: '0 10px 0 10px' }} onClick={searchLoop} />
+      <Button raised disabled={disabled} label="search" style={{ margin: '0 10px 0 10px' }} onClick={onButtonClick} />
       <LinearProgress closed={!disabled} progress={progressState} />
     </div>
   );
