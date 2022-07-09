0a3fa40786207cdaf3eb3b85a7433ff1f234b9d0
nate <nathangchay@gmail.com>
2022-07-09 03:52:15 -0600

    Add redux store provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import 'rmwc/dist/styles';
 
 import App from './components/App';
+import ReduxStore from './components/model/ReduxStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <App />,
+  <Provider store={ReduxStore}>
+    <App />
+  </Provider>,
 );
