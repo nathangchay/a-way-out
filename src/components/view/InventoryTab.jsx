4e7730297e19a7185c2f28906397261e270f9630
nate <nathangchay@gmail.com>
2022-07-09 03:52:08 -0600

    Use redux to get inventory

--- src/components/view/InventoryTab.jsx
+++ src/components/view/InventoryTab.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import { Typography } from '@rmwc/typography';
 
-import { getInventory } from '../model/Inventory';
-
 function InventoryTab() {
+  const inventory = useSelector((state) => state.inventory);
+
   return (
     <div className="block">
       <Typography use="headline6" className="tab-header">i have:</Typography>
-      {Object.entries(getInventory()).map(([key, value]) => (
+      {Object.entries(inventory).map(([key, value]) => (
         <Typography use="body2">
           {key}
           :
