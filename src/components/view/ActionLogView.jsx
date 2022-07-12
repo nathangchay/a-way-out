87cee9f30fb3ddef4e99eebab6fbef5d06c598ee
nate <nathangchay@gmail.com>
2022-07-11 18:01:13 -0600

    Use new actionLog state, add opacity to list

--- src/components/view/ActionLogView.jsx
+++ src/components/view/ActionLogView.jsx
@@ -3,13 +3,13 @@ import { useSelector } from 'react-redux';
 import { Typography } from '@rmwc/typography';
 
 function ActionLogView() {
-  const actionLog = useSelector((state) => state.actionLog.log);
+  const actionLog = useSelector((state) => state.actionLog);
 
   return (
     <div className="block">
       <Typography use="headline6" className="tab-header">my actions:</Typography>
-      {actionLog.map((action) => (
-        <Typography use="caption">{action}</Typography>
+      {actionLog.map((action, i) => (
+        <Typography use="caption" style={{ opacity: 1 / (i + 1) }}>{action}</Typography>
       ))}
     </div>
   );
