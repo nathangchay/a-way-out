437ea57a494e40d26488fef878a573fb4bf27e8d
nate <nathangchay@gmail.com>
2022-07-16 02:32:13 -0600

    Added key items section

--- src/components/view/InventoryTab.jsx
+++ src/components/view/InventoryTab.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Typography } from '@rmwc/typography';
 
+import Flashlight from '../controller/KeyItems';
+
+const keyItemsElements = {
+  flashlight: <Flashlight />,
+};
+
 function InventoryTab() {
   const inventory = useSelector((state) => state.inventory);
 
   return (
     <div className="block">
-      <Typography use="headline6" className="tab-header">i have:</Typography>
-      {Object.entries(inventory).map(([key, value]) => (
+      <Typography use="headline6" className="tab-header">key items:</Typography>
+      {Object.keys(inventory.keyItems).map((key) => (
+        keyItemsElements[key]
+      ))}
+      <Typography use="headline6" className="tab-header">resources:</Typography>
+      {Object.entries(inventory.resources).map(([key, value]) => (
         <Typography use="body2">
           {key}
           :
