4c855819864ff1017f93cfa2b398bfb82a6d8019
nate <nathangchay@gmail.com>
2022-07-16 02:31:30 -0600

    Added key item functionality

--- src/components/model/Inventory.js
+++ src/components/model/Inventory.js
@@ -2,16 +2,42 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const Inventory = createSlice({
   name: 'inventory',
-  initialState: {},
+  initialState: {
+    keyItems: {},
+    resources: {},
+  },
   reducers: {
-    addToInventory: (state, action) => {
+    addResource: (state, action) => {
       const { item, quantity } = action.payload;
 
-      if (!state[item]) {
-        return { ...state, [item]: quantity };
+      if (!state.resources[item]) {
+        state.resources[item] = quantity;
+      } else {
+        state.resources[item] += quantity;
       }
+    },
+    addKeyItem: (state, action) => {
+      const { item, data } = action.payload;
 
-      return { ...state, [item]: state[item] + quantity };
+      state.keyItems[item] = data;
+    },
+    _useKeyItem: (state, action) => {
+      const { itemAction } = action.payload;
+
+      if (itemAction === 'flashlight/use') {
+        const { flashlight } = state.keyItems;
+
+        if (flashlight.chargesLeft > 0) {
+          flashlight.chargesLeft -= 1;
+        }
+      } else if (itemAction === 'flashlight/recharge') {
+        const { resources } = state;
+
+        if (resources.battery) {
+          resources.battery -= 1;
+          state.keyItems.flashlight.chargesLeft = 5;
+        }
+      }
     },
     replaceInventory: (_, action) => {
       const { newState } = action.payload;
@@ -21,5 +47,7 @@ const Inventory = createSlice({
   },
 });
 
-export const { addToInventory, replaceInventory } = Inventory.actions;
+export const {
+  addResource, addKeyItem, _useKeyItem, replaceInventory,
+} = Inventory.actions;
 export default Inventory.reducer;
