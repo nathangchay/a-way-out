b1b2d91fc6b520141cc8812dcbb5253d59d53bc7
nate <nathangchay@gmail.com>
2022-07-20 04:08:05 -0600

    Add state control

--- src/components/controller/SettingsController.jsx
+++ src/components/controller/SettingsController.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { IconButton } from '@rmwc/icon-button';
 import {
   Dialog, DialogTitle, DialogContent, DialogActions, DialogButton,
@@ -6,8 +7,17 @@ import {
 import { ThemeProvider } from '@rmwc/theme';
 import { Switch } from '@rmwc/switch';
 
+import { toggleSetting } from '../model/Settings';
+
 function SettingsController() {
+  const dispatch = useDispatch();
+  const settings = useSelector((state) => state.settings);
+
   const [open, setOpen] = useState(false);
+
+  const onToggle = (setting) => {
+    dispatch(toggleSetting({ setting }));
+  };
 
   return (
     <div className="settings-container">
@@ -16,8 +26,8 @@ function SettingsController() {
         <Dialog open={open} onClose={() => setOpen(false)}>
           <DialogTitle>Settings</DialogTitle>
           <DialogContent style={{ display: 'flex', flexDirection: 'column' }}>
-            <Switch label="Dark theme" style={{ marginTop: 20 }} />
-            <Switch label="Autosave" style={{ marginTop: 20 }} />
+            <Switch label="Dark theme" checked={settings.darkTheme} onChange={() => onToggle('darkTheme')} style={{ marginTop: 20 }} />
+            <Switch label="Autosave" checked={settings.autosave} onChange={() => onToggle('autosave')} style={{ marginTop: 20 }} />
           </DialogContent>
           <DialogActions>
             <DialogButton action="close">close</DialogButton>
