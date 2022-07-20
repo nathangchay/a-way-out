1ed8e2cd546f16cf7b7c215e9c0690bb29a8b864
nate <nathangchay@gmail.com>
2022-07-20 04:08:23 -0600

    Add settings

--- src/components/model/ReduxStore.js
+++ src/components/model/ReduxStore.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import InventoryReducer, { _useKeyItem } from './Inventory';
 import ActionLogReducer from './ActionLog';
 import MapReducer from './Map';
+import SettingsReducer from './Settings';
 
 const ReduxStore = configureStore({
   reducer: {
     inventory: InventoryReducer,
     actionLog: ActionLogReducer,
     map: MapReducer,
+    settings: SettingsReducer,
   },
 });
 
