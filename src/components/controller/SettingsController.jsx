d53361a40da5b645516e107512f1f61ae6e42c17
nate <nathangchay@gmail.com>
2022-07-20 03:05:48 -0600

    Update capitalization, add close action

--- src/components/controller/SettingsController.jsx
+++ src/components/controller/SettingsController.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { IconButton } from '@rmwc/icon-button';
 import {
-  Dialog, DialogTitle, DialogContent, DialogActions,
+  Dialog, DialogTitle, DialogContent, DialogActions, DialogButton,
 } from '@rmwc/dialog';
 import { ThemeProvider } from '@rmwc/theme';
 import { Switch } from '@rmwc/switch';
@@ -14,12 +14,14 @@ function SettingsController() {
       <IconButton icon={{ icon: 'settings', size: 'xlarge' }} onClick={() => setOpen(true)} />
       <ThemeProvider options={{ primary: '#282c34', secondary: '#282c34', onSurface: '#282c34' }}>
         <Dialog open={open} onClose={() => setOpen(false)}>
-          <DialogTitle>settings</DialogTitle>
+          <DialogTitle>Settings</DialogTitle>
           <DialogContent style={{ display: 'flex', flexDirection: 'column' }}>
-            <Switch label="dark theme" style={{ marginTop: 20 }} />
-            <Switch label="autosave" style={{ marginTop: 20 }} />
+            <Switch label="Dark theme" style={{ marginTop: 20 }} />
+            <Switch label="Autosave" style={{ marginTop: 20 }} />
           </DialogContent>
-          <DialogActions />
+          <DialogActions>
+            <DialogButton action="close">close</DialogButton>
+          </DialogActions>
         </Dialog>
       </ThemeProvider>
     </div>
