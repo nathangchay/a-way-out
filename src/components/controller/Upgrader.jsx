a7b3ca91087e5f3546564edc28a685344f3cdebc
nate <nathangchay@gmail.com>
2022-07-23 17:49:43 -0600

    Add upgrade purchase functionality

--- src/components/controller/Upgrader.jsx
+++ src/components/controller/Upgrader.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Typography } from '@rmwc/typography';
 import { Button } from '@rmwc/button';
 
-function Upgrader({ upgradeName, curLevel, upgradeCost }) {
+import { purchaseUpgrade } from '../model/Research';
+
+function Upgrader({
+  upgradeName, type, curLevel, upgradeCost, maxed,
+}) {
+  const researchPoints = useSelector((state) => state.research.researchPoints);
+  const dispatch = useDispatch();
+
+  const onUpgradeClick = () => {
+    dispatch(purchaseUpgrade({ type, upgradeName }));
+  };
+
   return (
     <div className="container-upgrader">
       <div className="container-upgrader-info">
         <Typography use="body2">{`${upgradeName[0].toUpperCase() + upgradeName.substring(1)} (level ${curLevel})`}</Typography>
-        <Typography use="caption">{`Cost: ${upgradeCost} pts`}</Typography>
+        <Typography use="caption">{`Cost: ${maxed ? 'N/A' : `${upgradeCost} pts`}`}</Typography>
       </div>
-      <Button raised label="upgrade" style={{ margin: '0 10px 0 0' }} />
+      <Button raised disabled={maxed || researchPoints < upgradeCost} label={maxed ? 'max' : 'upgrade'} style={{ margin: '0 10px 0 0' }} onClick={onUpgradeClick} />
     </div>
   );
 }
