1b6fbc34e5690ac7e4ba83a23548d81535a7efb0
nate <nathangchay@gmail.com>
2022-07-23 22:45:51 -0600

    Add locked upgrade option

--- src/components/model/Research.js
+++ src/components/model/Research.js
@@ -3,12 +3,17 @@ import { createSlice } from '@reduxjs/toolkit';
 const Research = createSlice({
   name: 'research',
   initialState: {
-    researchPoints: 0,
+    researchPoints: 1000,
     generalResearch: {
       'search speed': {
         unlocked: true,
         curLevel: 1,
         costPerLevel: [10, 30, 70, 100],
+      },
+      'map movement': {
+        unlocked: false,
+        curLevel: 1,
+        costPerLevel: [100, 200, 300],
       },
     },
     keyItemUpgrades: {
